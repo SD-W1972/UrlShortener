1307aac5f11f7a0f7b213b38c633bfe8900563da
SD-W1972 <anthonybemol@gmail.com>
2026-04-16 10:15:31 -0300

    feat: implement JPA representation of click entity

---
+++ src/main/java/com/secon/UrlShortener/outbound/entities/JpaClickEntity.java
@@ -0,0 +1,36 @@
+package com.secon.UrlShortener.outbound.entities;
+
+import com.secon.UrlShortener.domain.model.ov.ClientInfo;
+import com.secon.UrlShortener.domain.model.ov.GeoLocationData;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Click")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JpaClickEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String originalUrl;
+    private String slug;
+    private LocalDateTime clickedAt;
+    private JpaClientInfoAdapter jpaClientInfoAdapter;
+    private JpaGeoLocationDataAdapter jpaGeoLocationDataAdapter;
+    private String ipAdress;
+
+}
