f1db73fab615239459eedc1607a4947322455bc5
SD-W1972 <anthonybemol@gmail.com>
2026-04-23 22:37:19 -0300

    feat: implement unit test for get original url service

--- src/main/java/com/secon/UrlShortener/application/port/in/service/GetOriginalUrlService.java
+++ src/main/java/com/secon/UrlShortener/application/port/in/service/GetOriginalUrlService.java
@@ -1,4 +1,12 @@
 package com.secon.UrlShortener.application.port.in.service;
 
-public class GetOriginalUrlService {
+import com.secon.UrlShortener.domain.model.Url;
+import com.secon.UrlShortener.domain.model.usecase.GetOriginalUrlUseCase;
+
+public class GetOriginalUrlService implements GetOriginalUrlUseCase {
+
+    @Override
+    public Url originalUrl(String slug) {
+        return null;
+    }
 }
--- src/test/java/com/secon/UrlShortener/application/usecases/GetOriginalUrlServiceTest.java
+++ src/test/java/com/secon/UrlShortener/application/usecases/GetOriginalUrlServiceTest.java
@@ -1,4 +1,50 @@
 package com.secon.UrlShortener.application.usecases;
 
+import com.secon.UrlShortener.application.port.in.service.GetOriginalUrlService;
+import com.secon.UrlShortener.application.port.out.UrlRepository;
+import com.secon.UrlShortener.domain.model.Url;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GetOriginalUrlServiceTest {
+
+    @Mock
+    UrlRepository urlRepository;
+
+    @InjectMocks
+    GetOriginalUrlService getOriginalUrlService;
+
+    Url url;
+
+    @BeforeEach
+    public void setup(){
+        url = new Url("https://google.com");
+        url.setSlug("dnh");
+    }
+
+    @Test
+    public void shouldReturnCorrectUrlFromSlug(){
+        when(urlRepository.findBySlug("dnh")).thenReturn(Optional.of(url));
+
+        Url urlFromService = getOriginalUrlService.originalUrl("dnh");
+        Assertions.assertNotNull(urlFromService);
+        Assertions.assertEquals(url, urlFromService);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSlugIsNotValid(){
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> getOriginalUrlService.originalUrl("00000000000000000000"));
+    }
 }
