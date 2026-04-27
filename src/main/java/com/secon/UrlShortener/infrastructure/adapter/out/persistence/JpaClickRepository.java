c9852837f92475ae81622e453d6e05c4050434b3
SD-W1972 <anthonybemol@gmail.com>
2026-04-27 09:54:23 -0300

    feat: implement port to jpa implementation of click repository

---
+++ src/main/java/com/secon/UrlShortener/infrastructure/adapter/out/persistence/JpaClickRepository.java
@@ -0,0 +1,17 @@
+package com.secon.UrlShortener.infrastructure.adapter.out.persistence;
+
+import com.secon.UrlShortener.infrastructure.adapter.out.persistence.entities.JpaClickEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaClickRepository extends JpaRepository<JpaClickEntity, Long> {
+    @Override
+    Optional<JpaClickEntity> findById(Long id);
+    @Override
+    List<JpaClickEntity> findAll();
+
+    void deleteById(Long id);
+
+}
