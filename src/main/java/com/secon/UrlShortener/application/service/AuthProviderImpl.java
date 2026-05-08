b3f72a5ad4a01a37e28063c6f0dc8684c87ce351
SD-W1972 <anthonybemol@gmail.com>
2026-05-08 09:26:25 -0300

    feat: implement auth provider adapter implementation class

--- src/main/java/com/secon/UrlShortener/application/service/AuthProviderImpl.java
+++ src/main/java/com/secon/UrlShortener/application/service/AuthProviderImpl.java
@@ -1,4 +1,45 @@
 package com.secon.UrlShortener.application.service;
 
-public class UserAuthProvider {
+import com.secon.UrlShortener.domain.model.User;
+import com.secon.UrlShortener.domain.model.enums.UserType;
+import com.secon.UrlShortener.domain.out.UserRepository;
+import com.secon.UrlShortener.domain.usecase.AuthProvider;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class AuthProviderImpl implements AuthProvider {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User register(String email, String password) {
+        if(userRepository.findByEmail(email).isPresent()){
+            throw new IllegalArgumentException("An account with this email already exists!");
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+
+        User user = new User(
+                email,
+                encodedPassword,
+                UserType.CLIENT,
+                new ArrayList<>()
+        );
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> login(String email, String password) {
+        return userRepository.findByEmail(email)
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()));
+    }
 }
