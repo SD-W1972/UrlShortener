8881d18cd10ecf56f3173ddedf02f1443c4880f8
SD-W1972 <anthonybemol@gmail.com>
2026-05-12 10:40:03 -0300

    feat: implement utility class ToStringList

--- src/main/java/com/secon/UrlShortener/application/utilities/ToStringList.java
+++ src/main/java/com/secon/UrlShortener/application/utilities/ToStringList.java
@@ -1,4 +1,14 @@
 package com.secon.UrlShortener.application.utilities;
 
+import com.secon.UrlShortener.domain.model.Click;
+
+import java.util.List;
+
 public class ToStringList {
+
+    public static List<String> toStringList(List<Click> clicks){
+        return clicks.stream()
+                .map(Click::toString)
+                .toList();
+    }
 }
