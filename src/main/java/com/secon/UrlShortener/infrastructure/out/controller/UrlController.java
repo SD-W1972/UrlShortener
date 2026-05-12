8dce9516ddf8002bb4ab378adfdba49825a664a7
SD-W1972 <anthonybemol@gmail.com>
2026-05-11 21:15:30 -0300

    feat: implement controller endpoints for getting slug

--- src/main/java/com/secon/UrlShortener/infrastructure/out/controller/UrlController.java
+++ src/main/java/com/secon/UrlShortener/infrastructure/out/controller/UrlController.java
@@ -1,4 +1,20 @@
 package com.secon.UrlShortener.infrastructure.out.controller;
 
+import com.secon.UrlShortener.domain.usecase.ShortenUrlUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class UrlController {
+
+    private final ShortenUrlUseCase shortenUrlUseCase;
+
+    public UrlController(ShortenUrlUseCase shortenUrlUseCase) {
+        this.shortenUrlUseCase = shortenUrlUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> getSlug(@RequestBody String originalUrl){
+        return ResponseEntity.status(201).body(shortenUrlUseCase.encodeToSlug(originalUrl));
+    }
 }
