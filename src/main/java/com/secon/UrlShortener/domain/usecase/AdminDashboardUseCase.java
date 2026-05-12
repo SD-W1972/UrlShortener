bafa11f8fcb85ee2418d8787b3461dd5e1930a9b
SD-W1972 <anthonybemol@gmail.com>
2026-05-12 10:14:23 -0300

    feat: implement port to admin dashboard

--- src/main/java/com/secon/UrlShortener/domain/usecase/AdminDashboardUseCase.java
+++ src/main/java/com/secon/UrlShortener/domain/usecase/AdminDashboardUseCase.java
@@ -1,5 +1,12 @@
 package com.secon.UrlShortener.domain.usecase;
 
+import com.secon.UrlShortener.domain.model.Click;
+
+import java.util.List;
+
 public interface AdminDashboardUseCase {
+
+    public List<Click> getDataByOriginalUrl(String originalUrl);
+    public List<Click> getAllData();
 
 }
