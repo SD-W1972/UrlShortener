897cc60ddd12ba74cc7e960ff928e2d121a36120
SD-W1972 <anthonybemol@gmail.com>
2026-05-25 11:16:08 -0300

    feat: implement admin dashboard controller

---
+++ src/main/java/com/secon/UrlShortener/infrastructure/out/controller/AdminDashboardController.java
@@ -0,0 +1,32 @@
+package com.secon.UrlShortener.infrastructure.out.controller;
+
+import com.secon.UrlShortener.domain.usecase.AdminDashboardUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+public class AdminDashboardController {
+
+    private final AdminDashboardUseCase adminDashboard;
+
+    public AdminDashboardController(AdminDashboardUseCase adminDashboard) {
+        this.adminDashboard = adminDashboard;
+    }
+
+    @GetMapping("/api/admin/dashboard")
+    public ResponseEntity<?> adminDashboard() {
+        return ResponseEntity.ok(Map.of("message", "Welcome to the dashboard!"));
+    }
+
+    @GetMapping("/api/admin/dashboard/{originalUrl}")
+    public ResponseEntity<?> getDataFromOriginalUrl(@PathVariable String originalUrl){
+        return ResponseEntity.status(200).body(adminDashboard.getDataByOriginalUrl(originalUrl));
+    }
+
+    @GetMapping("/api/admin/dashboard/all")
+    public ResponseEntity<?> getAllData(){
+        return ResponseEntity.status(200).body(adminDashboard.getAllData());
+    }
+}
